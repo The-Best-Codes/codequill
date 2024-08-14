eb35becc1376e641acae2c8ca913046ab4c59d92
The-Best-Codes <bestcodes.official@gmail.com>
2024-08-14 09:54:38 -0500

    Fix install script

--- .device_scripts/install.sh
+++ .device_scripts/install.sh
@@ -73,8 +73,8 @@ fi
 
 # Change directory
 print_step "Changing to the CodeQuill directory..."
-mkdir -p codequill/codequill
-cd codequill/codequill
+mkdir -p codequill
+cd codequill
 check_status
 
 # Install Node.js dependencies
