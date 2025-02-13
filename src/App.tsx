c22ac6e8748f0399a5d733d49166992ef4a8b19c
The-Best-Codes <bestcodes.official@gmail.com>
2025-02-12 19:18:41 -0600

    simple router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-export default function App() {
+import Home from "@/routes/home";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+function App() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Hello, World!</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
+
+export default App;
---
+++ src/routes/home/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-lg">Welcome to the Home Page!</h1>
+    </div>
+  );
+}
