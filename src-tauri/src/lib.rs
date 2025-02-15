5d518fd09a9df1a057235819ebe43067fdcfe13b
The-Best-Codes <bestcodes.official@gmail.com>
2025-02-15 17:46:00 -0600

    add single instance and deep link plugins

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,6 +7,13 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_deep_link::init())
+        .plugin(tauri_plugin_single_instance::init(|_app, _args, _cwd| {
+            // Handle the single instance event here.
+            // You can access the AppHandle, arguments, and current working directory.
+            // For example, to focus the main window:
+            // let _ = _app.get_webview_window("main").expect("no main window").set_focus();
+        }))
         .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
