0da06ba6dd707497ed82fddb2ec628f092ffc2ed
The-Best-Codes <bestcodes.official@gmail.com>
2025-02-20 18:08:45 -0600

    Experimental pt2: Sentry wizard config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
@@ -10,6 +11,11 @@ export default defineConfig(async () => ({
       babel: {
         plugins: [["babel-plugin-react-compiler", { target: "19" }]],
       },
+    }),
+    sentryVitePlugin({
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      org: "bestcodes-official",
+      project: "codequill-react",
     }),
   ],
 
@@ -36,45 +42,7 @@ export default defineConfig(async () => ({
     },
   },
 
-  /*
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          rehype: ["rehype-raw", "remark-gfm", "react-markdown"],
-          radix: [
-            "@radix-ui/react-accordion",
-            "@radix-ui/react-alert-dialog",
-            "@radix-ui/react-aspect-ratio",
-            "@radix-ui/react-avatar",
-            "@radix-ui/react-checkbox",
-            "@radix-ui/react-collapsible",
-            "@radix-ui/react-context-menu",
-            "@radix-ui/react-dialog",
-            "@radix-ui/react-dropdown-menu",
-            "@radix-ui/react-hover-card",
-            "@radix-ui/react-label",
-            "@radix-ui/react-menubar",
-            "@radix-ui/react-navigation-menu",
-            "@radix-ui/react-popover",
-            "@radix-ui/react-progress",
-            "@radix-ui/react-radio-group",
-            "@radix-ui/react-scroll-area",
-            "@radix-ui/react-select",
-            "@radix-ui/react-separator",
-            "@radix-ui/react-slider",
-            "@radix-ui/react-slot",
-            "@radix-ui/react-switch",
-            "@radix-ui/react-tabs",
-            "@radix-ui/react-toast",
-            "@radix-ui/react-toggle",
-            "@radix-ui/react-toggle-group",
-            "@radix-ui/react-tooltip",
-          ],
-          vendor: ["react", "react-dom", "react-router-dom"],
-        },
-      },
-    },
+    sourcemap: true,
   },
-  */
 }));
