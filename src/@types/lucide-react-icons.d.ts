dc357f974b72d1c7da7ae95f329b64630c29cc14
The-Best-Codes <bestcodes.official@gmail.com>
2025-06-10 11:34:27 -0500

    fix lucide direct import types (unsafely)

---
+++ src/@types/lucide-react-icons.d.ts
@@ -0,0 +1,17 @@
+declare module "lucide-react/dist/esm/icons/*" {
+  import { type LucideIcon } from "lucide-react";
+  const Icon: LucideIcon;
+  export default Icon;
+}
+
+declare module "lucide-react" {
+  import React from "react";
+  export interface LucideIcon
+    extends React.ForwardRefExoticComponent<
+      React.PropsWithoutRef<React.SVGProps<SVGSVGElement>> & {
+        title?: string;
+        size?: string | number;
+        absoluteStrokeWidth?: boolean;
+      } & React.RefAttributes<SVGSVGElement>
+    > {}
+}
