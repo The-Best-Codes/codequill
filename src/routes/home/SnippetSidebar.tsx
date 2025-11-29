02da7ad9b51aa9229197bb4200a6b30b66fbb8f4
The-Best-Codes <bestcodes.official@gmail.com>
2025-11-28 18:19:21 -0600

    rm uneeded mr-2

--- src/routes/home/SnippetSidebar.tsx
+++ src/routes/home/SnippetSidebar.tsx
@@ -310,7 +310,7 @@ const SnippetSidebar: React.FC<SnippetSidebarProps> = ({
             </div>
             <div className="px-2 flex flex-col gap-2">
               <Button variant="default" onClick={handleNewSnippetClick}>
-                <Plus className="h-4 w-4 mr-2" />
+                <Plus className="h-4 w-4" />
                 {t("newSnippet")}
               </Button>
               <Button
